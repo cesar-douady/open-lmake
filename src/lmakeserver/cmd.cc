5e224f03c4abce4cdbd2b3584a56a038013793e3
Cesar Douady <cesar.douady@doliam.fr>
2025-04-03 01:42:02 +0200

    improve lshow -d

--- src/lmakeserver/cmd.cc
+++ src/lmakeserver/cmd.cc
@@ -131,9 +131,11 @@ namespace Engine {
 
 	static void _audit_node( Fd fd , ReqOptions const& ro , bool verbose , Bool3 hide , ::string const& pfx , Node node , DepDepth lvl=0 ) {
 		Color color = Color::None ;
-		if      ( hide==Yes                                           ) color =                         Color::HiddenNote ;
-		else if ( !node->has_actual_job() && !is_target(node->name()) ) color = hide==No ? Color::Err : Color::HiddenNote ;
-		else if ( node->ok()==No                                      ) color =            Color::Err                     ;
+		if      ( hide==Yes                            ) color = Color::HiddenNote ;
+		else if ( node->ok()==No                       ) color = Color::Err        ;
+		else if ( node->crc==Crc::None                 ) color = Color::HiddenNote ;
+		else if ( !node->has_actual_job()              ) color = Color::Warning    ;
+		if      ( hide==No && color==Color::HiddenNote ) color = Color::None       ;
 		//
 		if ( verbose || color!=Color::HiddenNote ) {
 			if (ro.flags[ReqFlag::Quiet]) audit( fd , ro , color ,         mk_file(node->name()) , false/*as_is*/ , 0   ) ; // if quiet, no header, no reason to indent
