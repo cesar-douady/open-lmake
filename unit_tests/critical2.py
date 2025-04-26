781a7ae36c66dd35adb68cac921a3292ad00edca
Cesar Douady <cesar.douady@doliam.fr>
2025-04-26 19:28:39 +0200

    improve unit_tests : check static dep after critical

--- unit_tests/critical2.py
+++ unit_tests/critical2.py
@@ -17,17 +17,35 @@ if __name__!='__main__' :
 		target = r'dep{Digit:\d+}'
 		cmd    = ''
 
+
+	class GenAfter(Rule) :
+		target = r'after{Digit:\d+}'
+		cmd    = ''
+
 	class All(PyRule) :
 		target = 'dut'
-		deps = { 'LST' : ('lst','Critical') }
+		deps = {
+			'LST'   : ('lst'  ,'Critical')
+		,	'AFTER' :  'after2'
+		}
 		def cmd() :
 			from lmake import depend
 			for t in open(LST).readlines() :
 				depend(t.strip())
+			open('after1')
+			open('after2')
+			open('after3')
 
 else :
 
+	import os
+
 	import ut
 
-	print('dep1',file=open('lst','w')) ; ut.lmake( 'dut' , new    =1 , may_rerun=1 , done=1 , was_done  =1 )
+	print('dep1',file=open('lst','w')) ; ut.lmake( 'dut' , new    =2 , may_rerun=2 , done=5                )
 	print('dep2',file=open('lst','a')) ; ut.lmake( 'dut' , changed=1 , may_rerun=1 , done=1 , was_steady=1 )
+	os.unlink('after1')
+	os.unlink('after2')
+	os.unlink('after3')
+	print('dep1',file=open('lst','w'))
+	ut.lmake( 'dut' , changed=1 , may_rerun=2 , steady=4 ) # check after2 is built despite lst being modified
